638edffdd24364bbd028f78e6067df5d05583686
elprogamer456 <107517052+elprogamer456@users.noreply.github.com>
2022-06-22 13:49:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 alert('Para una mejor experiencia coloque la pantalla de forma horizontal.');
-$('#detect-button').click(function(){
-var detector = new MobileDetect(window.navigator.userAgent)
-console.log( "Mobile: " + detector.mobile());
-console.log( "Phone: " + detector.phone());
-console.log( "Tablet: " + detector.tablet());
-console.log( "OS: " + detector.os());
-console.log( "userAgent: " + detector.userAgent());
-});
